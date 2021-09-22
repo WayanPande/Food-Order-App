f93b2d603b5ad65d905b9380f5380de747536042
WayanPande <yande554@gmail.com>
2021-09-22 21:13:21 +0800

    Handling Errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -8,10 +8,16 @@ const AvailableMeals = () => {
 
     const [meals, setMeals] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState();
 
     useEffect(() => {
         const fetchMeals = async () => {
             const response = await fetch('https://react-http-efc92-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json');
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
             const responseData = await response.json();
 
             const loadedMeals = [];
@@ -27,11 +33,20 @@ const AvailableMeals = () => {
             setMeals(loadedMeals);
             setIsLoading(false);
         };
-        fetchMeals();
+
+        fetchMeals().catch(error => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        });
+
     }, []);
 
     if (isLoading) {
         return <section className={styles.MealsLoading}><p>Loading...</p></section>
+    }
+
+    if (httpError) {
+        return <section className={styles.MealsError}><p>{httpError}</p></section>
     }
 
     const mealsList = meals.map(meal => <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
